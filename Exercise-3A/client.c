265ab0b2ed3d207c9127e1dcc77d7381a8b22aae
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2024-01-14 17:11:59 +0100

    wip ab3

--- Exercise-3A/client.c
+++ Exercise-3A/client.c
@@ -21,30 +21,28 @@
 
 static void usage();
 
+bool isValidHTTPHeader(const char *response);
+
 /**
  * Extract hostName and filePath from the url
  * @param url Url to parse.
  * @param hostName hostName to store at
  * @param filePath filePath to store at
  */
-void parseUrl(const char *url, char *hostName, char *filePath) {
+void parseUrl(const char *url, char *hostName, char *filePath)
+{
     char protocol[8];
     sscanf(url, "%7[^:]://%[^;/?:@=&]%s", protocol, hostName, filePath);
-    if (strcmp(protocol, "http") != 0) {
+    if (strcmp(protocol, "http") != 0)
+    {
         fprintf(stderr, "Error: Only HTTP protocol is supported.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (filePath[0] == '/') {
+    if (filePath[0] == '/')
+    {
         memmove(filePath, filePath + 1, strlen(filePath)); // Verschiebe den String um 1 nach links
     }
-
-//    if (filePath[strlen(filePath) - 1] == '\001' || filePath[strlen(filePath) - 1] == '\0') {
-//        strcpy(filePath, "index.html");
-//    }
-//    else if (filePath[strlen(filePath) - 1] == '/') {
-//        strcat(filePath, "index.html");
-//    }
 }
 
 char *programName;
@@ -56,11 +54,13 @@ char *programName;
  * @param argv The optional arguments.
  * @return EXIT_SUCCESS or EXIT_FAILURE.
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     programName = argv[0];
 
     // Parse command line arguments
-    if (argc < 2) {
+    if (argc < 2)
+    {
         usage();
     }
 
@@ -74,59 +74,69 @@ int main(int argc, char *argv[]) {
     bool fileSet = false;
     bool dirSet = false;
 
-    while ((opt = getopt(argc, argv, "p:o:d:")) != -1) {
-        switch (opt) {
-            case 'p':
-                if (portAlreadySet) {
-                    usage();
-                }
-                portAlreadySet = true;
-                char *endptr;
-                long tempPort = strtol(optarg, &endptr, 10);
-
-                if (endptr == optarg || *endptr != '\0') {
-                    usage();
-                }
-
-                if (tempPort < 0 || tempPort > 65535) {
-                    fprintf(stderr, "Error: Port number out of range (0-65535)\n");
-                    usage();
-                }
-
-                port = (int) tempPort;
-                break;
-            case 'o':
-                if (fileSet || dirSet) {
-                    usage();
-                }
-                fileSet = true;
-                outputFile = optarg;
-                break;
-            case 'd':
-                if (fileSet || dirSet) {
-                    usage();
-                }
-                dirSet = true;
-                outputDirectory = optarg;
-
-                // check if folder has no points in name
-                const char *ptr = outputDirectory;
-                while (*ptr) {
-                    if (*ptr == '.') {
-                        fprintf(stderr, "Invalid folder name\n");
-                        usage();
-                        exit(EXIT_FAILURE);
-                    }
-                    ptr++;
-                }
-
-                break;
-            default:
+    while ((opt = getopt(argc, argv, "p:o:d:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            if (portAlreadySet)
+            {
                 usage();
+            }
+            portAlreadySet = true;
+            char *endptr;
+            long tempPort = strtol(optarg, &endptr, 10);
+
+            if (endptr == optarg || *endptr != '\0')
+            {
+                usage();
+            }
+
+            if (tempPort < 0 || tempPort > 65535)
+            {
+                fprintf(stderr, "Error: Port number out of range (0-65535)\n");
+                usage();
+            }
+
+            port = (int)tempPort;
+            break;
+        case 'o':
+            if (fileSet || dirSet)
+            {
+                usage();
+            }
+            fileSet = true;
+            outputFile = optarg;
+            break;
+        case 'd':
+            if (fileSet || dirSet)
+            {
+                usage();
+            }
+            dirSet = true;
+            outputDirectory = optarg;
+
+            // check if folder has no points in name
+            const char *ptr = outputDirectory;
+            while (*ptr)
+            {
+                if (*ptr == '.')
+                {
+                    fprintf(stderr, "Invalid folder name\n");
+                    usage();
+                    exit(EXIT_FAILURE);
+                }
+                ptr++;
+            }
+
+            break;
+        default:
+            usage();
         }
     }
 
-    if ((argc - optind) != 1) {
+    if ((argc - optind) != 1)
+    {
         usage();
     }
 
@@ -145,7 +155,8 @@ int main(int argc, char *argv[]) {
 
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    if (sockfd < 0)
+    {
         fprintf(stderr, "Error opening socket\n");
         return EXIT_FAILURE;
     }
@@ -157,14 +168,16 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
 
     int status = getaddrinfo(hostname, NULL, &hints, &serverInfo);
-    if (status != 0) {
+    if (status != 0)
+    {
         fprintf(stderr, "Error getting address info: %s\n", gai_strerror(status));
         return EXIT_FAILURE;
     }
 
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    if (sockfd < 0)
+    {
         perror("Error opening socket");
         return EXIT_FAILURE;
     }
@@ -173,10 +186,11 @@ int main(int argc, char *argv[]) {
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr = ((struct sockaddr_in *) serverInfo->ai_addr)->sin_addr;
+    serverAddr.sin_addr = ((struct sockaddr_in *)serverInfo->ai_addr)->sin_addr;
 
     // Connect to server
-    if (connect(sockfd, (const struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+    if (connect(sockfd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
         perror("Error connecting");
         return EXIT_FAILURE;
     }
@@ -187,9 +201,9 @@ int main(int argc, char *argv[]) {
     char request[BUFFER_SIZE];
     sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", filepath, hostname);
 
-//    printf("%s", request);
     // Send HTTP request
-    if (send(sockfd, request, strlen(request), 0) < 0) {
+    if (send(sockfd, request, strlen(request), 0) < 0)
+    {
         perror("Error sending request");
         return EXIT_FAILURE;
     }
@@ -199,69 +213,84 @@ int main(int argc, char *argv[]) {
     int statusCode = -1;
 
     FILE *outputStream = stdout;
-    if (fileSet || dirSet) {
-        if (dirSet) {
-            char outputPath[strlen(outputDirectory) + 256 + 1];
-            if (outputDirectory != NULL) {
-                strcpy(outputPath, outputDirectory);
-                if(filepath[0] == '\0' || filepath[0] == '?') {
-                    strcat(outputPath, "/index.html");
-                }
-                else {
-                    if(filepath[0] != '/') {
-                        strcat(outputPath, "/");
-                    }
-                    strcat(outputPath, filepath);
-                }
-
-                if (mkdir(outputDirectory, 0777) != 0) {
-                    if (errno != EEXIST) {
-                        fprintf(stderr, "Cannot create mkdir: %s.\n", outputDirectory);
-                        exit(EXIT_FAILURE);
-                    }
-                }
+    if (dirSet)
+    {
+        char outputPath[strlen(outputDirectory) + 256 + 1];
+        if (outputDirectory != NULL)
+        {
+            strcpy(outputPath, outputDirectory);
+            if (filepath[0] == '\0' || filepath[0] == '?')
+            {
+                strcat(outputPath, "/index.html");
             }
-
-            outputStream = fopen(outputPath, "w+");
-            if (outputStream == NULL) {
-                fprintf(stderr, "Cannot open file: %s.\n", outputPath);
-                exit(EXIT_FAILURE);
+            else if (filepath[0] != '/')
+            {
+                strcat(outputPath, "/");
             }
-        } else {
-            outputStream = fopen(outputFile, "w+");
-            if (outputStream == NULL) {
-                fprintf(stderr, "Cannot open file: %s.\n", outputFile);
+            strcat(outputPath, filepath);
+        }
+
+        if (mkdir(outputDirectory, 0777) != 0)
+        {
+            if (errno != EEXIST)
+            {
+                fprintf(stderr, "Cannot create mkdir: %s.\n", outputDirectory);
                 exit(EXIT_FAILURE);
             }
         }
+
+        outputStream = fopen(outputPath, "w+");
+        if (outputStream == NULL)
+        {
+            fprintf(stderr, "Cannot open file: %s.\n", outputPath);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (fileSet)
+    {
+        outputStream = fopen(outputFile, "w+");
+        if (outputStream == NULL)
+        {
+            fprintf(stderr, "Cannot open file: %s.\n", outputFile);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    while ((bytesReceived = recv(sockfd, buffer, BUFFER_SIZE - 1, 0)) > 0) {
+    while ((bytesReceived = recv(sockfd, buffer, BUFFER_SIZE - 1, 0)) > 0)
+    {
         buffer[bytesReceived] = '\0';
 
         // Check for the end of headers (empty line)
         char *body_start = strstr(buffer, "\r\n\r\n");
-        if (body_start != NULL) {
+        if (body_start != NULL)
+        {
             body_start += 4; // Move past the empty line
             fwrite(body_start, sizeof(char), bytesReceived - (body_start - buffer), outputStream);
-        } else {
+        }
+        else
+        {
             fwrite(buffer, sizeof(char), bytesReceived, outputStream);
         }
 
         // Check response status code
-        if (statusCode == -1) {
+        if (statusCode == -1)
+        {
             char *statusLine = strtok(buffer, "\r\n");
-            if (strncmp(statusLine, "HTTP/1.1", 8) == 0) {
+            if (isValidHTTPHeader(statusLine))
+            {
                 int parsedStatusCode = -1;
                 sscanf(statusLine, "%*s %d", &parsedStatusCode);
                 statusCode = parsedStatusCode;
-            } else {
+            }
+            else
+            {
                 fprintf(stderr, "Protocol error!\n");
                 exit(2);
             }
 
-            // Check if status is not 200 OK
-            if (statusCode != 200) {
+            // Check if status is not 200
+            if (statusCode != 200)
+            {
                 fprintf(stderr, "%s\n", statusLine + 9); // Printing status text
                 exit(3);
             }
@@ -271,7 +300,8 @@ int main(int argc, char *argv[]) {
     // Close socket and file
     close(sockfd);
 
-    if (outputFile != NULL) {
+    if (outputFile != NULL)
+    {
         fclose(outputStream);
     }
 
@@ -283,7 +313,33 @@ int main(int argc, char *argv[]) {
  * @brief Prints a usage info for the program and exits with EXIT_FAILURE
  * @return exits the programm with EXIT_FAILURE
  */
-void usage() {
+void usage()
+{
     fprintf(stderr, "Usage: %s [-p PORT] [-o FILE | -d DIR] URL\n", programName);
     exit(EXIT_FAILURE);
+}
+
+/**
+ * Checks the HTTP header.
+ * @return true if the http header is valid
+ */
+bool isValidHTTPHeader(const char *response)
+{
+    // Überprüfe, ob der Header mit "HTTP/1.1 " beginnt
+    if (strncmp(response, "HTTP/1.1 ", 9) != 0)
+    {
+        return false;
+    }
+
+    // Versuche, die Zahl nach "HTTP/1.1 " zu parsen
+    char *endptr;
+    strtol(response + 9, &endptr, 10);
+
+    // Überprüfe, ob strtol erfolgreich war und ob das Ende des Strings erreicht wurde
+    if (endptr == response + 9)
+    {
+        return false;
+    }
+
+    return true;
 }
--- Exercise-3B/server.c
+++ Exercise-3B/server.c
@@ -24,24 +24,161 @@
 
 char *programName;
 
+void signalHandler(int signum);
+void handleRequest(int clientSocket, const char *documentRoot, const char *indexFile);
+void usage();
+
+/**
+ * Program entry point.
+ * @details Serves the contents in a given folder.
+ * @param argc The argument counter.
+ * @param argv The optional arguments.
+ * @return EXIT_SUCCESS or EXIT_FAILURE.
+ */
+int main(int argc, char *argv[])
+{
+    programName = argv[0];
+    if (argc < 2)
+    {
+        usage();
+    }
+
+    // Default values
+    int port = 8080;
+    const char *indexFile = "index.html";
+    const char *documentRoot;
+
+    // Parse command-line arguments
+    int opt;
+    bool portAlreadySet = false;
+    bool indexFileAlreadySet = false;
+    while ((opt = getopt(argc, argv, "p:i:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            if (portAlreadySet)
+            {
+                usage();
+            }
+            portAlreadySet = true;
+
+            char *endptr;
+            long tempPort = strtol(optarg, &endptr, 10);
+
+            if (endptr == optarg || *endptr != '\0')
+            {
+                usage();
+            }
+
+            if (tempPort < 0 || tempPort > 65535)
+            {
+                fprintf(stderr, "Error: Port number out of range (0-65535)\n");
+                usage();
+            }
+
+            port = (int)tempPort;
+            break;
+        case 'i':
+            if (indexFileAlreadySet)
+            {
+                usage();
+            }
+            indexFileAlreadySet = true;
+            indexFile = optarg;
+            break;
+        default:
+            usage();
+        }
+    }
+
+    if (optind >= argc)
+    {
+        usage();
+    }
+
+    documentRoot = argv[optind];
+
+    // Set up signal handlers
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
+
+    // Create socket
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t addr_len = sizeof(clientAddress);
+
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Socket creation failed");
+        return EXIT_FAILURE;
+    }
+
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    // Bind socket
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
+        perror("Binding failed");
+        close(serverSocket);
+        return EXIT_FAILURE;
+    }
+
+    // Listen for connections
+    if (listen(serverSocket, BACKLOG) == -1)
+    {
+        perror("Listen failed");
+        close(serverSocket);
+        return EXIT_FAILURE;
+    }
+
+    printf("Server listening on port %d\n", port);
+
+    while (1)
+    {
+        // Accept incoming connections
+        if ((clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &addr_len)) == -1)
+        {
+            perror("Accept failed");
+            close(serverSocket);
+            return EXIT_FAILURE;
+        }
+
+        printf("Connection from %s:%d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
+
+        // Handle request
+        handleRequest(clientSocket, documentRoot, indexFile);
+    }
+
+    close(serverSocket);
+
+    return EXIT_SUCCESS;
+}
+
 /**
  * Program usage info.
  * @brief Prints a usage info for the program and exits with EXIT_FAILURE
  * @return exits the programm with EXIT_FAILURE
  */
-void usage() {
+void usage()
+{
     fprintf(stderr, "Usage: %s [-p PORT] [-o FILE | -d DIR] URL\n", programName);
     exit(EXIT_FAILURE);
 }
 
-void handleRequest(int clientSocket, const char *documentRoot, const char *indexFile) {
+void handleRequest(int clientSocket, const char *documentRoot, const char *indexFile)
+{
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
     char method[10], path[255], protocol[20];
 
     // Read the request from the client
     bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
-    if (bytesRead < 0) {
+    if (bytesRead < 0)
+    {
         perror("Error reading from socket");
         close(clientSocket);
         return;
@@ -55,7 +192,8 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
     sscanf(buffer, "%s %s %s", method, path, protocol);
 
     // Support only GET method
-    if (strlen(method) == 0 || strlen(path) == 0 || strcmp(protocol, "HTTP/1.1") != 0) {
+    if (strlen(method) == 0 || strlen(path) == 0 || strcmp(protocol, "HTTP/1.1") != 0)
+    {
         char *response = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\n\r\n";
         send(clientSocket, response, strlen(response), 0);
         close(clientSocket);
@@ -63,7 +201,8 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
     }
 
     // Support only GET method
-    if (strcmp(method, "GET") != 0) {
+    if (strcmp(method, "GET") != 0)
+    {
         char *response = "HTTP/1.1 501 Not Implemented\r\nConnection: close\r\n\r\n";
         send(clientSocket, response, strlen(response), 0);
         close(clientSocket);
@@ -75,7 +214,8 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
     memset(fileRequested, 0, sizeof(fileRequested));
 
     strcpy(fileRequested, path);
-    if (path[strlen(path) - 1] == '/') {
+    if (path[strlen(path) - 1] == '/')
+    {
         strcat(fileRequested, indexFile);
     }
 
@@ -84,7 +224,8 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
 
     // Check if file exists
     int filefd = open(filePath, O_RDONLY);
-    if (filefd < 0) {
+    if (filefd < 0)
+    {
         char *response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n";
         send(clientSocket, response, strlen(response), 0);
         close(clientSocket);
@@ -106,7 +247,8 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
     send(clientSocket, responseHeader, strlen(responseHeader), 0);
 
     // Send file content
-    while ((bytesRead = read(filefd, buffer, BUFFER_SIZE)) > 0) {
+    while ((bytesRead = read(filefd, buffer, BUFFER_SIZE)) > 0)
+    {
         send(clientSocket, buffer, bytesRead, 0);
     }
 
@@ -114,124 +256,9 @@ void handleRequest(int clientSocket, const char *documentRoot, const char *index
     close(clientSocket);
 }
 
-void signalHandler(int signum) {
+void signalHandler(int signum)
+{
     printf("Signal received. Closing server.\n");
 
     exit(0);
-}
-
-/**
- * Program entry point.
- * @details Serves the contents in a given folder.
- * @param argc The argument counter.
- * @param argv The optional arguments.
- * @return EXIT_SUCCESS or EXIT_FAILURE.
- */
-int main(int argc, char *argv[]) {
-    programName = argv[0];
-    if (argc < 2) {
-        usage();
-    }
-
-    // Default values
-    int port = 8080;
-    const char *indexFile = "index.html";
-    const char *documentRoot;
-
-    // Parse command-line arguments
-    int opt;
-    bool portAlreadySet = false;
-    bool indexFileAlreadySet = false;
-    while ((opt = getopt(argc, argv, "p:i:")) != -1) {
-        switch (opt) {
-            case 'p':
-                if (portAlreadySet) {
-                    usage();
-                }
-                portAlreadySet = true;
-
-                char *endptr;
-                long tempPort = strtol(optarg, &endptr, 10);
-
-                if (endptr == optarg || *endptr != '\0') {
-                    usage();
-                }
-
-                if (tempPort < 0 || tempPort > 65535) {
-                    fprintf(stderr, "Error: Port number out of range (0-65535)\n");
-                    usage();
-                }
-
-                port = (int) tempPort;
-                break;
-            case 'i':
-                if (indexFileAlreadySet) {
-                    usage();
-                }
-                indexFileAlreadySet = true;
-                indexFile = optarg;
-                break;
-            default:
-                usage();
-        }
-    }
-
-    if (optind >= argc) {
-        usage();
-    }
-
-    documentRoot = argv[optind];
-
-    // Set up signal handlers
-    signal(SIGINT, signalHandler);
-    signal(SIGTERM, signalHandler);
-
-    // Create socket
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddress, clientAddress;
-    socklen_t addr_len = sizeof(clientAddress);
-
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket creation failed");
-        return 1;
-    }
-
-    memset(&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(port);
-
-    // Bind socket
-    if (bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
-        perror("Binding failed");
-        close(serverSocket);
-        return 1;
-    }
-
-    // Listen for connections
-    if (listen(serverSocket, BACKLOG) == -1) {
-        perror("Listen failed");
-        close(serverSocket);
-        return 1;
-    }
-
-    printf("Server listening on port %d\n", port);
-
-    while (1) {
-        // Accept incoming connections
-        if ((clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &addr_len)) == -1) {
-            perror("Accept failed");
-            close(serverSocket);
-            return 1;
-        }
-
-        printf("Connection from %s:%d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
-
-        // Handle request
-        handleRequest(clientSocket, documentRoot, indexFile);
-    }
-
-    close(serverSocket);
-
-    return 0;
 }
